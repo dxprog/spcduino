cf718d35eb6181b8ad7d58575a5d70e7044fdbcb
Matt Hackmann <matt@dxprog.com>
2019-05-21 22:52:44 -0700

    beginning to implement serial protocol

--- main.ino
+++ main.ino
@@ -9,44 +9,25 @@
 #define NOP __asm__ __volatile__ ("nop\n\t")
 
 // Commands that can be issued by spc-player
-#define CMD_RESET        0
-#define CMD_LOAD_DSP     1
-#define CMD_WRITE_CHUNK  2
-#define CMD_PLAY         3
+#define CMD_RESET        1
+#define CMD_LOAD_DSP     2
+#define CMD_WRITE_CHUNK  3
+#define CMD_PLAY         4
 
 // Responses to send back to spc-player
-#define RSP_OKAY         0
-#define RSP_FAIL         1
+#define RSP_OKAY         1
+#define RSP_FAIL         2
+#define RSP_BAD_CHECKSUM 3
+#define RSP_READY        86
 
 #define STATE_INIT  0
 #define STATE_READY 1
 #define STATE_TXFR  2
 
-// DSP boot loader
-static byte dspLoader[] =
-{ //For loading the 128 byte DSP ram. DO NOT CHANGE.
-  0xC4, 0xF2,       //START:  Mov [0F2h], A
-  0x64, 0xF4,       //LOOP:   Cmp A, [0F4h]
-  0xD0, 0xFC,       //        Bne LOOP
-  0xFA, 0xF5, 0xF3, //        Mov [0F3h], [0F5h]
-  0xC4, 0xF4,       //        Mov [0F4h], A
-  0xBC,             //        Inc A
-  0x10, 0xF2,       //        Bpl START
-
-  0x8F, 0xFF, 0xFC, //      Mov [0FCh], #timer_2
-  0x8F, 0xFF, 0xFB, //      Mov [0FBh], #timer_1
-  0x8F, 0x10, 0xFA, //      Mov [0FAh], #timer_0
-
-  0xCD, 0xF9,       //      Mov X, #stack_pointer
-  0xBD,             //      Mov SP, X
-
-  0x2F, 0xAB,       //        Bra 0FFC9h  ;Right when IPL puts AA-BB on the IO ports and waits for CC.
-};
+#define DSP_LOADER_SIZE   28
+#define DSP_DATA_SIZE     128
 
 byte state = STATE_INIT;
-byte lastPort0 = 0;
-byte currentAddress = 0;
-byte currentDirection = 2;
 
 void setup() {
   pinMode(READ, OUTPUT);
@@ -55,58 +36,104 @@ void setup() {
   pinMode(PA0, OUTPUT);
   pinMode(PA1, OUTPUT);
 
-  Serial.flush();
-  Serial.begin(148986);
-  Serial.println("Begin");
+  Serial.begin(115200);
+  Serial.write(RSP_READY);
 }
 
-void readSerialBuffer(byte *buffer, unsigned short len) {
-  int bufferPos = 0;
-  int checksum = 0;
+/**
+ * Reads len amount of bytes off the serial port
+ *
+ * @param {uint8_t} buffer Pointer to the buffer to write the serial data to
+ * @param {uint16_t} len The amount of bytes to read
+ * @return {bool} If the transfer went okay
+ */
+uint8_t read_serial_buffer(uint8_t *buffer, uint16_t len) {
+  uint16_t bufferPos = 0;
+  uint8_t checksum = 0;
   while (bufferPos < len) {
-    while (Serial.available() < 32);
-    for (int i = 0; i < 32; i++) {
-      buffer[bufferPos] = Serial.read();
-      checksum = (checksum + buffer[bufferPos]) & 0xFF;
-      bufferPos++;
-    }
+    while (Serial.available() == 0);
+    buffer[bufferPos] = Serial.read();
+    checksum = (checksum + buffer[bufferPos]) & 0xFF;
+    bufferPos++;
   }
 
-  Serial.print("Read 0x");
-  Serial.print(bufferPos, HEX);
-  Serial.print(" bytes into buffer with a checksum: ");
-  Serial.println(checksum);
+  // The last byte should be the expected checksum
+  while (Serial.available() == 0);
+  uint8_t expectedChecksum = Serial.read();
+
+  // If the checksum doesn't _check out_, send a bad checksum response
+  if (checksum != expectedChecksum) {
+    Serial.write(RSP_BAD_CHECKSUM);
+  }
+
+  return checksum == expectedChecksum;
 }
 
-void initDsp() {
+/**
+ * Resets the SPC
+ */
+void cmd_reset() {
+  spc_reset();
+  Serial.write(RSP_OKAY);
+}
+
+/**
+ * Reads the DSP loader program and register data from serial
+ * and then writes it to the SPC
+ *
+ * @param {uint8_t[28]} loaderProgram The DSP loader program
+ * @param {uint8_t[128]} dspRegisters The DSP register data
+ */
+void cmd_load_dsp() {
+  // Receive the DSP loader
+  uint8_t loaderProgram[28];
+  uint8_t expectedChecksum;
+  if (!read_serial_buffer(loaderProgram, 28)) {
+    return;
+  }
+
+  // Send success
+  Serial.write(RSP_OKAY);
+
   // Receive the DSP data
-  byte buffer[128];
-  Serial.println("Receiving DSP data");
-  readSerialBuffer(buffer, 128);
+  uint8_t dspRegisters[128];
+  if (!read_serial_buffer(dspRegisters, 128)) {
+    return;
+  }
 
   // Write the DSP loader
   spc_begin_transfer(0x0002);
-  spc_write_chunk(dspLoader, 28);
-
-  Serial.println("DSP loader written");
-
-  // Bit twiddling
-  buffer[0x4C] = 0x00;
-  buffer[0x6C] = 0x60;
+  spc_write_chunk(loaderProgram, 28);
 
   // End the transfer and run the DSP loader
   spc_end_transfer(0x0002);
-  spc_write_chunk(buffer, 127);
 
-  // Write the last byte and wait for IPL ROM to cycle back around
-  spc_write(PORT_1, buffer[127]);
-  spc_write(PORT_0, 127);
+  // Now write the DSP register data
+  spc_write_chunk(dspRegisters, 128);
+
+  // Wait for the loader to finish and IPL ROM to kick back in
   spc_zero_wait(0xAA);
 
-  Serial.println("DSP data written");
+  Serial.write(RSP_OKAY);
 }
 
 void loop() {
+
+  // Wait for a command to show up on serial
+  if (Serial.available() > 0) {
+    uint8_t command = Serial.read();
+
+    switch (command) {
+      case CMD_RESET:
+        cmd_reset();
+        break;
+      case CMD_LOAD_DSP:
+        cmd_load_dsp();
+        break;
+    }
+  }
+
+  /*
   if (state == STATE_INIT) {
     spc_reset();
     state = STATE_READY;
@@ -157,15 +184,18 @@ void loop() {
     Serial.print("SPC data written (");
     Serial.print(checksum);
     Serial.println("). Starting playback");
-    spc_end_transfer(0xFF90);
+    spc_end_transfer(0xE6D0);
     Serial.println("I've done all I can, boss");
+
+    spc_write(PORT_3, 0x01);
+    spc_write(PORT_0, 0x01);
 
     unsigned short bail = 512;
     while (spc_read(PORT_0) != 0x53 && --bail > 0);
 
-    spc_write(PORT_0, 0x73);
-    spc_write(PORT_1, 0xFE);
-    spc_write(PORT_2, 0xFF);
+    spc_write(PORT_0, 0x00);
+    spc_write(PORT_1, 0x00);
+    spc_write(PORT_2, 0x00);
     spc_write(PORT_3, 0x00);
 
     if (bail == 0) {
@@ -174,4 +204,5 @@ void loop() {
     }
     state = 100;
   }
+  */
 }
